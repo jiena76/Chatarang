e8f095143da4223a951ee465249b8a944683dbe2
jiena76 <jieun@purdue.edu>
2018-06-12 15:08:24 -0400

    add MessageForm.js and add form submission feature

---
+++ src/MessageForm.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+
+const MessageForm = () => {
+  return(
+    <form>
+      <input 
+        type="text"
+        name='body'
+        placeholder='Type a message...'
+      />
+      <button type='button'>Send</button>
+    </form>
+  );
+}
+
+export default MessageForm;
