3f7f402f60450cc90a3adafa72cbf750b3a4560f
jiena76 <jieun@purdue.edu>
2018-06-12 14:39:23 -0400

    add messages to the messageList

--- src/MessageList.js
+++ src/MessageList.js
@@ -1,9 +1,23 @@
 import React, { Component } from 'react'
+import { EMSGSIZE } from 'constants';
 
 const MessageList = () => {
+  const messages = [
+    {
+      id: 1,
+      userName: 'dstrus',
+      body: 'Chatting up a storm.'
+    },
+    {
+      id: 2,
+      userName: 'dplazzo',
+      body: 'This guy is so annoying, I hate my job.'
+    }
+  ]
+
   return(
     <div className="MessageList">
-      MessageList
+      {messages.map(mssg => <div>{mssg.userName}: {mssg.body}</div>)}
     </div>
   )
 }
