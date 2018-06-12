37ae4ff585e8acc9abd04083afb4767014773bb9
jiena76 <jieun@purdue.edu>
2018-06-12 14:33:08 -0400

    add MessageList.js

---
+++ src/MessageList.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+const MessageList = () => {
+  return(
+    <div className="MessageList">
+      MessageList
+    </div>
+  )
+}
+
+export default MessageList;
