51ddcdaf0ac5dbbeb29e757324d1b86cb365e029
jiena76 <jieun@purdue.edu>
2018-06-12 15:13:16 -0400

    move messages variable from MessageList.js to Chat.js as local variable

--- src/Chat.js
+++ src/Chat.js
@@ -5,11 +5,30 @@ import MessageList from './MessageList'
 import MessageForm from './MessageForm'
 
 class Chat extends Component{
+  constructor(){
+    // getting constructor from the parent class
+    super()
+
+    this.state = {
+      messages: [
+        {
+          id: 1,
+          userName: 'dstrus',
+          body: 'Strawberry.'
+        },
+        {
+          id: 2,
+          userName: 'dplazzo',
+          body: 'This guy is so annoying, I hate my job.'
+        }
+      ]
+    }
+  }
   render(){
     return(
       <div className="Chat">
         <ChatHeader />
-        <MessageList />
+        <MessageList messages={this.state.messages} />
         <MessageForm />
       </div>
     );
--- src/MessageList.js
+++ src/MessageList.js
@@ -2,23 +2,10 @@ import React, { Component } from 'react'
 
 import Message from './Message'
 
-const MessageList = () => {
-  const messages = [
-    {
-      id: 1,
-      userName: 'dstrus',
-      body: 'Chatting up a storm.'
-    },
-    {
-      id: 2,
-      userName: 'dplazzo',
-      body: 'This guy is so annoying, I hate my job.'
-    }
-  ]
-
+const MessageList = (props) => {
   return(
     <div className="MessageList">
-      {messages.map(mssg => (
+      {props.messages.map(mssg => (
         <Message key={mssg.id} message={mssg} />
       ))}
     </div>
