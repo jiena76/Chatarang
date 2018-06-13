f0b0f7373299b77416d54c860a71cbb91d654559
jiena76 <jieun@purdue.edu>
2018-06-12 23:28:11 -0400

    add styles to messageForm.js

--- src/MessageForm.js
+++ src/MessageForm.js
@@ -17,18 +17,69 @@ class MessageForm extends Component{
 
   render(){
     return(
-      <form className="MessageForm" onSubmit={this.handleSubmit}>
+      <form 
+      className="MessageForm"
+      onSubmit={this.handleSubmit}
+      style={styles.messageForm}>
+
         <input 
           type="text"
           name='body'
           placeholder='Type a message...'
           value={this.state.body}
           onChange={this.handleChange}
+          style={styles.input}
         />
-        <button type='submit'>Send</button>
+        <button type='submit' style={styles.button}>Send</button>
       </form>
     );
   }
 }
 
 export default MessageForm;
+
+const styles = {
+
+  //.MessageForm
+  messageForm: {
+    backgroundColor: "white",
+    height: "3rem",
+    display: "flex",
+    alignItems: "stretch",
+    border: "2px solid #999",
+    borderRadius: "0.5rem",
+    margin: "0.25rem",
+    padding: "0"
+},
+  //.MessageForm .chatIcon
+  chatIcon: {
+    display: "flex",
+    borderRadius: "0.5rem",
+    alignItems: "center",
+    backgroundColor: "white",
+    color: "#ccc",
+    padding: "0 0.5rem",
+    fontSize: "1.2rem"
+},
+  //.MessageForm input
+  input: {
+    flex: "1",
+    fontSize: "1.2rem",
+    border: "0"
+},
+  //.MessageForm input:focus
+  inputFocus: {
+    outline: "0"
+},
+  //.MessageForm button
+  button: {
+    fontSize: "1.5rem",
+    backgroundColor: "#1A8FE3",
+    color: "white",
+    paddingLeft: "1rem",
+    paddingRight: "1rem",
+    borderTopRightRadius: "0.5rem",
+    borderBottomRightRadius: "0.5rem",
+    border: "1px solid white"
+  }
+}
