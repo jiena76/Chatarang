e57c3bff41a72b6d4ae9d68b46b89660b4ea5ea0
jiena76 <jieun@purdue.edu>
2018-06-21 05:06:04 -0400

    add room remove button

--- src/RoomLink.js
+++ src/RoomLink.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { StyleSheet, css } from 'aphrodite';
+import base from './base'
 
 const RoomLink = ({room, setRoom}) => {
   const handleClick = (e) => {
@@ -7,12 +8,23 @@ const RoomLink = ({room, setRoom}) => {
     setRoom(room);
   };
 
+  const removeRoom = (e) => {
+    e.preventDefault();
+    base.remove(room.name);
+    base.remove(`rooms/${room.name}`);
+  }
+
   return (
     <li className={css(styles.li)}>
-      {/* not "this.handleClick" because this component is not a class */}
-      <a href="/" className={css(styles.liA)} onClick={handleClick}>
-        {room.name}
-      </a>
+      <div className={css(styles.liDiv)}>
+        {/* not "this.handleClick" because this component is not a class */}
+        <a href="/" className={css(styles.liA)} onClick={handleClick}>
+          {room.name}
+        </a>
+        <button className={css(styles.button)} onClick={removeRoom}>
+          <i className="fas fa-times"></i>
+        </button>
+      </div>
     </li>
   )
 }
@@ -24,11 +36,18 @@ const styles = StyleSheet.create({
   li: {
     marginBottom: "0.5rem"
   },
+  liDiv: {
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
   //.RoomList li a
   liA: {
     display: "block",
     color: "whitesmoke",
     textDecoration: "none",
+    borderRadius: "5px",
+    padding: "0px 5px 2px 2px",
 
     ":before": {
       content: "'# '"
@@ -36,6 +55,20 @@ const styles = StyleSheet.create({
 
     ":hover": {
       backgroundColor: "rgba(255, 255, 255, 0.2)"
+    }
+  },
+  button: {
+    border: 0,
+    backgroundColor: 'transparent',
+    outline: 0,
+    padding: 0,
+    fontSize: '1rem',
+    color: 'transparent',
+    cursor: 'pointer',
+    transition: 'color 0.25s ease-out',
+
+    ':hover': {
+      color: 'rgba(255,255,255, 0.7)',
     }
   },
 });
