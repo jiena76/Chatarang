cf4422661f21aa1aaa1c18748a74f6858c092c6b
jiena76 <jieun@purdue.edu>
2018-06-30 19:21:22 -0400

    fix comments

--- src/index.js
+++ src/index.js
@@ -8,8 +8,7 @@ import {BrowserRouter as Router, Route} from 'react-router-dom';
 // base of the application, connects url router option with the app
 ReactDOM.render(
   <Router>
-    {/* instead of doing <App />,
-    doing this below will add "history", location", and "match" in "props" */}
+    {/* instead of doing <App />, doing this below will add "history", location", and "match" in "props" */}
     <Route component={App} />
   </Router>, 
   document.getElementById('root')
