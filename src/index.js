9431555106bfdd3ab44a34d236f6f0111c75cde0
jiena76 <jieun@purdue.edu>
2018-06-29 22:27:01 -0400

    add React Router

--- src/index.js
+++ src/index.js
@@ -3,12 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import {BrowserRouter as Router} from 'react-router-dom';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
 
 // base of the application, connects url router option with the app
 ReactDOM.render(
   <Router>
-    <App />
+    {/* instead of just doing <App />,
+    doing this below will add "history", location", and "match" in "props" */}
+    <Route component={App} />
   </Router>, 
   document.getElementById('root')
 );
