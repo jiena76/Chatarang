3153772082ab95389b773c8e0712732a06177ef1
jiena76 <jieun@purdue.edu>
2018-07-30 16:57:55 -0400

    edit how the room edit form is hidden

--- src/Main.js
+++ src/Main.js
@@ -70,11 +70,22 @@ class Main extends Component{
       context: this, 
       state: "rooms", 
       then: () => {
-        if(this.state.idList[this.props.match.params.roomName])
-          this.setRoom(this.state.rooms[this.state.idList[this.props.match.params.roomName]]);
+        if(this.state.idList[this.props.match.params.roomName]){
+          // roomExists = ID of the room
+          const roomExists = this.state.idList[this.props.match.params.roomName];
+          this.setRoom(this.state.rooms[roomExists]);
+
+          // base.listenTo(`/rooms/${roomExists}`, {
+          //   context: this,
+          //   then: () => {
+          //     //this.props.history.push(`/rooms/${this.state.rooms[roomExists]}`);
+          //     console.log(this.state.rooms[roomExists]);
+          //   }
+          // });
+        }
         // handles when the requested room is not available
         else
-          this.props.history.push(`/rooms/General`);
+          this.props.history.push("/rooms/General");
       },
     });
     /*
